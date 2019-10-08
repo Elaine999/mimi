ef4a3e08c9eed799a45c29619ab4f1a7f7bdd51e
zhangtin <zhangtin@163.com>
2019-10-08 11:44:24 +0800

    mmm

--- xiaomi/public/js/index.js
+++ xiaomi/public/js/index.js
@@ -1,7 +1,6 @@
-var swiperWrapperimg=$('.swiperWrapper');//获取图片
-var index=0;
-var len=swiperWrapperimg.length;//获取图片个数
-var smallpoints=$('.swiper_pagination_ul>li');//获取小圆点
+
+
+
 
 // 显示隐藏
 $('.siteCategoryOne>ul>li').hover(function(){
@@ -20,7 +19,13 @@ function(){
 )
 
 
-
+;(function(){
+    var _global;
+    function move(){
+        var swiperWrapperimg=$('.swiperWrapper');//获取图片
+        var index=0;
+        var len=swiperWrapperimg.length;//获取图片个数
+        var smallpoints=$('.swiper_pagination_ul>li');//获取小圆点
 // 背景图片切换-上一张
 $('.swiper_button_prev').on('click',function(){
     index--;
@@ -28,6 +33,7 @@ $('.swiper_button_prev').on('click',function(){
         index=4;
     }
     $(swiperWrapperimg[index]).fadeIn('slow').siblings('.swiperWrapper').fadeOut('fast');
+    $(smallpoints[index]).addClass('smallShow').siblings().removeClass('smallShow')
 })
 // 背景图片切换-下一张
 $('.swiper_button_next').on('click',function(){
@@ -36,8 +42,9 @@ $('.swiper_button_next').on('click',function(){
         index=0;
     }
     $(swiperWrapperimg[index]).fadeIn('slow').siblings('.swiperWrapper').fadeOut('fast');
+    $(smallpoints[index]).addClass('smallShow').siblings().removeClass('smallShow')
 })
-// 点击切换到相应的图片
+// 点击切换到相应的图片-按钮
 smallpoints.on('click',function(){
     var lis=event.target;
     index=$(lis).data('idx');
@@ -45,6 +52,7 @@ smallpoints.on('click',function(){
     $(smallpoints[index]).addClass('smallShow').siblings().removeClass('smallShow')
 
 })
+
 //自动播放
     var timeId=setInterval(function(){
         index++;
@@ -53,20 +61,38 @@ smallpoints.on('click',function(){
         }
         $(swiperWrapperimg[index]).fadeIn('slow').siblings('.swiperWrapper').fadeOut('fast');
         $(smallpoints[index]).addClass('smallShow').siblings().removeClass('smallShow')
-    },4000)
+        console.log(status)
+    },4000) 
 //移入停止！！！
-// swiperWrapperimg.mouseenter(function(){
-//     window.clearInterval(timeId);
-// })
-// swiperWrapperimg.mouseenter(function(){
-// })
+swiperWrapperimg.mouseover(function(){
+    window.clearInterval(timeId);
+})
+//移除启动定时器
+swiperWrapperimg.mouseout(function(){
+    timeId=setInterval(function(){
+        index++;
+        if(index>len-1){
+            index=0;
+        }
+        $(swiperWrapperimg[index]).fadeIn('slow').siblings('.swiperWrapper').fadeOut('fast');
+        $(smallpoints[index]).addClass('smallShow').siblings().removeClass('smallShow')
+    },4000)
+})
+    }
+ _global=(function(){return this;}())
+ !(move in _global) && (_global.move = move);
+}())
+
+new move();
 
 //搜索框
 $('#search').focus(function(){
     $('#search').css({borderColor:'#ff7600'});
     $('.search_btn').css({borderColor:'#ff7600'});
+    $('.search_hot_words').hide();
 })
 $('#search').blur(function(){
     $('#search').css({borderColor:'rgb(224, 224, 224)'});
     $('.search_btn').css({borderColor:'rgb(224, 224, 224)'});
+    $('.search_hot_words').show();
 })
