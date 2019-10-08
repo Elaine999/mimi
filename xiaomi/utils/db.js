4112a3072c64246bbb71d1d6e7071766596ec6b3
gaosongsong <gaosongsong@163.com>
2019-10-08 11:01:09 +0800

    依赖，直接安装

---
+++ xiaomi/utils/db.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql')
+
+module.exports = {
+    config: {
+        host: '94.191.87.109',
+        port: 3306,
+        user: 'root',
+        password: 'hs6j58q',
+        database: 'level3_1'
+    },
+    dbConnect:function (sql, sqlArr, callBack) {
+        var pool = mysql.createPool(this.config)
+        pool.getConnection(function (err, connect) {
+            if (err) {
+                console.log(err);
+                return err;
+            }
+            connect.query(sql, sqlArr, callBack)
+            connect.release()
+        })
+    }
+}
