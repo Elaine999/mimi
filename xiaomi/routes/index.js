a2ac2085e65530d38f6d1c11a9212a8a009ea2f5
2962616639@qq.com <2962616639@qq.com>
2019-10-08 17:42:47 +0800

    注册（数据库）缺邮箱验证

--- xiaomi/routes/index.js
+++ xiaomi/routes/index.js
@@ -14,4 +14,20 @@ router.get('/123',function(req,res){
     res.send(data);
   })
 })
+
+router.post("/reg.jsp", function (req, res, next) {
+  let Email = req.body.eMail
+  let Password = req.body.regPwd
+  let sql = "INSERT INTO userdetails(Email,Password) VALUES(?,?)"
+  let sqlARR = [Email,Password]
+  console.log(sqlARR);
+
+  db.dbConnect(sql, sqlARR, function (err, data) {
+    if (err) {
+      res.send("注册失败请检查")
+    } else {
+      res.send("注册成功")
+    }
+  })
+})
 module.exports = router;
