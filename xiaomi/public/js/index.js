4ecaa7f7d7f688c60cec68ba5536ee8fdb949862
gaosongsong <gaosongsong@163.com>
2019-10-10 09:05:18 +0800

    搜索

--- xiaomi/public/js/index.js
+++ xiaomi/public/js/index.js
@@ -17,17 +17,6 @@ function(){
 }
 )
 
-//退出的a标签
-$('#logout').on('click',function(e){
-    e.preventDefault();
-    $.ajax({
-        url:'/login/logout.jsp',
-        type:'post',
-        success:function(res){
-            window.location.href = 'http://127.0.0.1:3000/';
-        }
-    })
-})
 
 
 ;(function(){
