a783a98de52da0a8e7fb7917377655a1bed5d1c3
2962616639@qq.com <2962616639@qq.com>
2019-10-10 19:41:12 +0800

    购物车删除出数据库

--- xiaomi/public/js/car.js
+++ xiaomi/public/js/car.js
@@ -4,36 +4,35 @@
 
 $("body").click(function(e){
    if(e.target.className=="del"){
-      console.log($(e.target).parent());
+      let CiD= e.target.dataset.cid
       $(e.target).parent().remove()
 
       $.ajax({
         url: "/car/car.jsp",
         type: 'get',
+        data:{
+            cid:CiD
+        },
         success:function(res){
-            console.log(res);
-            
+            // console.log(res[0].Pid);
         }
       })
    }
     
     
 })
-
-
     $.ajax({
         url: "/car/car.php",
         type: 'get',
         success:function(res) {
-            console.log(res[0].Pimg);
+            // console.log(res[0].Pimg);
             let sum = 0
             for (var i = 0; i < res.length; i++) {
-                
                 sum=parseInt(sum)+ parseInt(res[i].Ppri) 
                 phoneBox.innerHTML += `
                 <ul>
                 <li>&nbsp;</li>
-                <li><img src="${res[i].Pimg}"></li>
+                <li><img style="height: 80px;width: 80px" src="${res[i].Pimg}"></li>
                 <li> ${res[i].Pname} </li>
                 <li> ${res[i].Ppri} </li>
                 <li class="inputBoxs">
@@ -42,7 +41,7 @@ $("body").click(function(e){
                     <input type="button" value="+">
                 </li>
                 <li>${res[i].Ppri}</li>
-                <li class="del">X</li>
+                <li class="del" data-cid="${res[i].Cid}">X</li>
             </ul>
                 `
             }
