f7a407f888df80def33f4c47a6f76261922b6908
gaosongsong <gaosongsong@163.com>
2019-10-10 22:43:34 +0800

    gss

--- xiaomi/routes/com.js
+++ xiaomi/routes/com.js
@@ -13,8 +13,27 @@ var db = require('./../utils/db.js');
 router.post('/insert', function (req, res, next) {
     let data = req.body;
 
-    res.send('123');
+    res.send(data);
 });
+
+router.get('/search',function(req,res){
+    let pid = req.query.pid;
+    let sql = `select * from comtable join userdetails on comtable.uid=userdetails.UID where comtable.pid=?`
+    let sqlArr = [pid]
+    db.dbConnect(sql,sqlArr,function(err,data){
+        console.log(data);
+        if (err) {
+            res.send({state:false,mas:'服务器错误'})
+        }
+        if (data.length>0) {
+            res.send({data,state:true})
+        }else{
+            res.send({state:false})
+        }
+    })
+
+
+})
 
 
 module.exports = router;
