6302c8fb917acd2697f55bcc6ba4bff693cae501
2962616639@qq.com <2962616639@qq.com>
2019-10-10 20:44:04 +0800

    购物车完成

--- xiaomi/public/js/car.js
+++ xiaomi/public/js/car.js
@@ -1,36 +1,37 @@
     let phoneBox = document.querySelector(".phoneBox")
-   let payBox=document.querySelector(".payBox")
+    let payBox = document.querySelector(".payBox")
 
 
-$("body").click(function(e){
-   if(e.target.className=="del"){
-      let CiD= e.target.dataset.cid
-      $(e.target).parent().remove()
+    $("body").click(function (e) {
+        if (e.target.className == "del") {
+            let CiD = e.target.dataset.cid
+            $(e.target).parent().remove()
 
-      $.ajax({
-        url: "/car/car.jsp",
-        type: 'get',
-        data:{
-            cid:CiD
-        },
-        success:function(res){
-            // console.log(res[0].Pid);
+            $.ajax({
+                url: "/car/car.jsp",
+                type: 'get',
+                data: {
+                    cid: CiD
+                },
+                success: function (res) {
+                    // console.log(res[0].Pid);
+                }
+            })
         }
-      })
-   }
-    
-    
-})
+
+
+    })
     $.ajax({
         url: "/car/car.php",
         type: 'get',
-        success:function(res) {
-            console.log(res[0].Uid);
+        success: function (res) {
+            // console.log(res[0].Uid);
             let sum = 0
-            // if(res[0].Uid=)
             for (var i = 0; i < res.length; i++) {
-                sum=parseInt(sum)+ parseInt(res[i].Ppri) 
-                phoneBox.innerHTML += `
+                if (res[i].Uid == userId) {
+
+                    sum = parseInt(sum) + parseInt(res[i].Ppri)
+                    phoneBox.innerHTML += `
                 <ul>
                 <li>&nbsp;</li>
                 <li><img style="height: 80px;width: 80px" src="${res[i].Pimg}"></li>
@@ -45,14 +46,15 @@ $("body").click(function(e){
                 <li class="del" data-cid="${res[i].Cid}">X</li>
             </ul>
                 `
-            }
-            payBox.innerHTML=`
+                }
+                payBox.innerHTML = `
             <p>共${res.length}件商品，已选择${res.length}件</p>
             <span>合计<b>${sum}</b>元</span>
             <div class="right">
             去结算
             </div>
             `
+            }
         }
 
     })
