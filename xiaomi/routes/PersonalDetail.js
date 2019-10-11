a0df49be057edd9d47f924c6a487fbb7421c7cea
zhangtin <zhangtin@163.com>
2019-10-11 08:57:51 +0800

    张婷-图片存入数据库

--- xiaomi/routes/PersonalDetail.js
+++ xiaomi/routes/PersonalDetail.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var db=require('./../utils/db.js');
 
-router.get('/345',function(req,res){
-    let baseimg=req.query.img;
+router.post('/345',function(req,res){
+    let baseimg=req.body.img;
+    // console.log(baseimg,99999);
     let sql='UPDATE userdetails SET Avatar=? WHERE UID=1'
     db.dbConnect(sql,[baseimg],function(error,data){
+        
         res.send(data);
     })
 })
