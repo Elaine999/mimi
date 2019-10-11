dd61ca49f51f6fcb7d255fead36f39f07f652664
zhangtin <zhangtin@163.com>
2019-10-11 13:11:43 +0800

    zhangting-资料修改页面

--- xiaomi/routes/PersonalDetail.js
+++ xiaomi/routes/PersonalDetail.js
@@ -12,4 +12,25 @@ router.post('/345',function(req,res){
     })
 })
 
+router.get('/',function(req,res){
+    let sql='select Mobile,Email,Avatar from userdetails where uid= ?';
+    let Mobile=null;
+    let Email=null;
+    let Avatar=null;
+    let Address=null;
+    let Gender=1;
+    let Nickname=null;
+    db.dbConnect(sql,[req.session.uid],function(err,data){
+    if(data.length>0){
+        Mobile=data[0].Mobile;
+        Email=data[0].Email;
+        Avatar=data[0].Avatar;
+        Address=data[0].Address;
+        Gender=data[0].Gender;
+        Nickname=data[0].Nickname;
+    }
+    console.log(Mobile,Email,9999999999)
+    res.render('PersonalDetails.ejs',{loginState:req.session.loginState,username:req.session.username,uid:req.session.uid,mobile:Mobile,email:Email,avatar:Avatar,address:Address,gender:Gender,nickname:Nickname})        
+    })
+})
 module.exports = router;
--- xiaomi/routes/personalCenter.js
+++ xiaomi/routes/personalCenter.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var db=require('./../utils/db.js');
 
 router.get('/',function(req,res){
-    console.log(12345677);
-    
-    res.render('PersonalCenter.ejs',{loginState:req.session.loginState,username:req.session.username,uid:req.session.uid,mobile:'18398261695',email:null})
+    let sql='select Mobile,Email,Avatar from userdetails where uid= ?';
+    let Mobile=null;
+    let Email=null;
+    let Avatar=null;
+    db.dbConnect(sql,[req.session.uid],function(err,data){
+    if(data.length>0){
+        Mobile=data[0].Mobile;
+        Email=data[0].Email;
+        Avatar=data[0].Avatar;
+    }
+    console.log(Mobile,Email,9999999999)
+    res.render('PersonalCenter.ejs',{loginState:req.session.loginState,username:req.session.username,uid:req.session.uid,mobile:Mobile,email:Email,avatar:Avatar})        
+    })
 })
 
 module.exports = router;
