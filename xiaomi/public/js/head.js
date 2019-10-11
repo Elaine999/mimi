0d7dc7a656d9eee202fc99a9d1203301c1b5a91c
gaosongsong <gaosongsong@163.com>
2019-10-11 19:41:42 +0800

    xiegai big

--- xiaomi/public/js/head.js
+++ xiaomi/public/js/head.js
@@ -3,4 +3,4 @@ var list = consumer.querySelector("ul");
 var container = document.querySelector(".container");
 var aside = container.querySelector("aside");
 var personalInfo = container.querySelector(".personalInfo");
-$(list).width($(consumer).width() - 4);
+// $(list).width($(consumer).width() - 4);
--- xiaomi/public/js/logout.js
+++ xiaomi/public/js/logout.js
@@ -5,7 +5,29 @@ $('#logout').on('click',function(e){
         url:'/login/logout.jsp',
         type:'post',
         success:function(res){
-            window.location.href = 'http://127.0.0.1:3000/';
+            let prizeBox = document.createElement("div")
+            $(prizeBox).css({
+                width: " 300px",
+                height: "100px",
+                background: "rgba(101, 101, 102, 0.4)",
+                color: "#fff",
+                fontSize: "20px",
+                textAlign: "center",
+                lineHeight: "100px",
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                marginLeft: '-150px',
+                marginTop: '-50px'
+            })
+            prizeBox.innerText = `退出成功`
+            document.body.appendChild(prizeBox)
+            var timer=setInterval(function(){
+                document.body.removeChild(prizeBox)
+            },2000)
+            setTimeout(function(){
+                window.location.href = 'http://127.0.0.1:3000/';
+            },500)
         }
     })
 })
