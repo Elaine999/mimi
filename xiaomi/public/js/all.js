5353addf4b816f84d74da21beea94abf6fbad28f
2962616639@qq.com <2962616639@qq.com>
2019-10-11 11:09:18 +0800

    购物车和订单页面整合

--- xiaomi/public/js/all.js
+++ xiaomi/public/js/all.js
@@ -7,7 +7,7 @@
             let CiD = e.target.dataset.cid
             // $(e.target).parent().remove()
             console.log(e.target);
-            
+
             $.ajax({
                 url: "/all/all.jsp",
                 type: 'get',
@@ -25,33 +25,35 @@
     $.ajax({
         url: "/all/all.php",
         type: 'get',
-        data:{ruid:userId},
+        data: {
+            ruid: userId
+        },
         success: function (res) {
             // console.log(res[0]);
             let sum = 0
             for (var i = 0; i < res.length; i++) {
                 // console.log(res[i]);
-                
-                if (res[i].Uid == userId) {
 
-                    sum = parseInt(sum) + parseInt(res[i].Ppri)
+                if (res[i].uid == userId) {
+
+                    sum = parseInt(sum) + parseInt(res[i].ppri)
                     phoneBox.innerHTML += `
                 <ul>
                 <li>&nbsp;</li>
-                <li><img style="height: 80px;width: 80px" src="${res[i].Pimg}"></li>
-                <li> ${res[i].Pname} </li>
-                <li> ${res[i].Ppri}元</li>
+                <li><img style="height: 80px;width: 80px" src="${res[i].pimg}"></li>
+                <li> ${res[i].pname} </li>
+                <li> ${res[i].ppri}元</li>
                 <li class="inputBoxs">
                     <input type="button" value="-">
                     <input type="text" value="1" class="textBtns">
                     <input type="button" value="+">
                 </li>
-                <li>${res[i].Ppri}元</li>
-                <li><input data-cid="${res[i].Cid}" class="del" type="button" value="去评价" style="outline: none;background: orangered;color: white;text-align: center;border:none;width: 60px;height: 40px;font-size:14px;"></li>
+                <li>${res[i].ppri}元</li>
+                <li><input data-cid="${res[i].cid}" class="del" type="button" value="去评价" style="outline: none;background: orangered;color: white;text-align: center;border:none;width: 60px;height: 40px;font-size:14px;"></li>
             </ul>
                 `
                 }
-            
+
             }
         }
 
